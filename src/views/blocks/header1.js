3f24c7f7499641e11564b58ac0a9ab64e17a9e17
emanoelsaraivas@gmail.com <emanoelsaraivas@gmail.com>
2024-02-02 10:55:02 -0300

    alteração do header1 e header2

--- src/views/blocks/header1.js
+++ src/views/blocks/header1.js
@@ -1,8 +1,8 @@
 const hbs = `
-<div class="container text-center pt-5 pb-5">
-  <h5>{{title}}</h5>
-  <h1 class="display-4">{{tagline}}</h1>
-  <a class="btn btn-link" href="#">{{link}}</a>
+<div class="container text-center pt-5 pb-5" style="background-color: {{backgroundColor}}">
+  <h5 style="color: {{colorTitle}}">{{title}}</h5>
+  <h1 class="display-4" style="color: {{colorTagLine}}">{{tagline}}</h1>
+  <a class="btn btn-link" href="{{href}}" style="color: {{colorLink}}">{{link}}</a>
 </div>
 `;
 
@@ -14,20 +14,45 @@ const block = {
   defaultData: {
     title: "Hello World",
     tagline: "Lorem ipsum dolor sit amet.",
-    link: "Read more",
+    link: "Saiba mais",
+    href: "http://google.com",
+    backgroundColor: "#fff",
+    colorLink: "#007BEE",
+    colorTitle: "#000",
+    colorTagLine: "#000",
   },
   config: {
     title: {
       type: "string",
-      name: "Title",
+      name: "Título",
+    },
+    colorTitle: {
+      type: "color",
+      name: "Cor do seu título",
     },
     tagline: {
       type: "string",
       name: "Tag Line",
     },
+    colorTagLine: {
+      type: "color",
+      name: "Cor da sua tag",
+    },
     link: {
       type: "string",
-      name: "Text on the link",
+      name: "Texto para seu link",
+    },
+    href: {
+      type: "string",
+      name: "Link para mais informações ou compra",
+    },
+    colorLink: {
+      type: "color",
+      name: "Cor do seu link",
+    },
+    backgroundColor: {
+      type: "color",
+      name: "Cor de fundo",
     },
   },
 };
--- src/views/blocks/header2.js
+++ src/views/blocks/header2.js
@@ -1,10 +1,10 @@
 const hbs = `
 <div class="jumbotron m-0 rounded-0" style="background-color: {{backgroundColor}}">
   <div class="container text-center ">
-    <h1 class="display-4">{{title}}</h1>
-    <p class="lead text-muted">{{tagline}}</p>
-    <a class="btn btn-primary my-2" href="{{href}}">{{button}}</a>
-    <a class="btn btn-link my-2" href="{{href}}">{{link}}</a>
+    <h1 class="display-4" style="color: {{colorTitle}}">{{title}}</h1>
+    <p class="lead" style="color: {{colorTagLine}}">{{tagline}}</p>
+    <a class="btn btn-primary my-2" href="{{hrefButton}}" style="background-color: {{backgroundButton}}; border-color: {{backgroundButton}}">{{button}}</a>
+    <a class="btn btn-link my-2" href="{{href}}" style="color: {{colorLink}}">{{link}}</a>
   </div>
 </div>
 `;
@@ -18,26 +18,55 @@ const block = {
     title: "Hello World",
     tagline: "Lorem ipsum dolor sit amet.",
     button: "Click here",
-    link: "Read more",
+    backgroundButton: "#0069D9",
+    link: "Saiba Mais",
     backgroundColor: "#fff",
     href: "http://google.com",
+    hrefButton: "http://google.com",
+    colorTitle: "#000",
+    colorTagLine: "#7A8489",
+    colorLink: "#007BEE",
   },
   config: {
     title: {
       type: "string",
       name: "Title",
     },
+    colorTitle: {
+      type: "color",
+      name: "Cor do título",
+    },
     tagline: {
       type: "string",
       name: "Tag Line",
+    },
+    colorTagLine: {
+      type: "color",
+      name: "Cor da tag",
     },
     button: {
       type: "string",
       name: "Text on the button",
     },
+    hrefButton: {
+      type: "string",
+      name: "Link do botão",
+    },
+    backgroundButton: {
+      type: "color",
+      name: "Cor de fundo do botão",
+    },
     link: {
       type: "string",
-      name: "Text on the link",
+      name: "Texto do link",
+    },
+    href: {
+      type: "string",
+      name: "Link do saiba mais",
+    },
+    colorLink: {
+      type: "color",
+      name: "Cor do link",
     },
     backgroundColor: {
       type: "color",
